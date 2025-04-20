16d3367538da4574f0860881ae61258575b02148
Faseeh <faseeh1080@gmail.com>
2025-04-20 18:13:27 +0530

    Move programs to 'programs' folder

--- generate_readme.py
+++ generate_readme.py
@@ -1,17 +1,25 @@
 import os
 
+programs_folder = "programs"
 ignored_folders = ["z-old", ".git", "__pycache__", "node_modules", ".vscode"]
 
 
 def generate_readme():
     readme_content = "# Example Programs\n\n"
 
-    for folder in os.listdir("."):
-        if folder in ignored_folders or not os.path.isdir(folder):
+    if not os.path.exists(programs_folder):
+        print(f"'{programs_folder}' directory does not exist.")
+        return
+
+    for folder in os.listdir(programs_folder):
+        if folder in ignored_folders:
+            continue
+
+        if not os.path.isdir(os.path.join(programs_folder, folder)):
             continue
 
         folder_name = folder.replace("-", " ").lower()
-        readme_content += f"- [{folder_name}](/{folder}/)\n"
+        readme_content += f"- [{folder_name}](/{programs_folder}/{folder}/)\n"
 
     with open("README.md", "w") as readme_file:
         readme_file.write(readme_content)
