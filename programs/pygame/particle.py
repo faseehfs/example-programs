545257f2dc323316993b248d1895f7e05db040e9
Faseeh School <faseeh1080@gmail.com>
2025-08-05 14:25:11 +0530

    Create particle.py

--- programs/pygame/particle.py
+++ programs/pygame/particle.py
@@ -2,12 +2,15 @@ import pygame
 from pygame.math import Vector2
 
 class Drop():
-    def __init__(self, position: Vector2, velocity: Vector2, length: float):
+    def __init__(self, position: Vector2, length: float, speed: float):
         self.position = position
-        self.velocity = velocity
         self.length = length
+        self.speed = speed
     
+        self.velocity = None
+
     def refresh(self, surface: pygame.Surface):
+        self.velocity = (Vector2(pygame.mouse.get_pos()) - self.position).normalize() * self.speed
         self.position += self.velocity * dt
         pygame.draw.line(
             surface, (255, 255, 255),
@@ -21,7 +24,7 @@ clock = pygame.time.Clock()
 dt = 0
 running = True
 
-drop = Drop(Vector2(0, 0), Vector2(10, 10), 5)
+drop = Drop(Vector2(0, 0), 5, 100)
 
 while running:
     for event in pygame.event.get():
@@ -34,4 +37,5 @@ while running:
 
     pygame.display.flip()
     dt = clock.tick(30) / 1000
+
 pygame.quit()
