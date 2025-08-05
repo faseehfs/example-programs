c528d7e57676037cac377c84c0c8f6ca00875c9b
FaseehFS <faseeh1080@gmail.com>
2025-08-05 21:07:39 +0530

    Format particle.py

--- programs/pygame/particle.py
+++ programs/pygame/particle.py
@@ -1,24 +1,35 @@
 import pygame
 from pygame.math import Vector2
 
-class Drop():
-    def __init__(self, position: Vector2, speed: float=100, length: float=4, width: float=2):
+
+class Drop:
+    def __init__(
+        self,
+        position: Vector2,
+        speed: float = 100,
+        length: float = 4,
+        width: float = 2,
+    ):
         self.position = position
         self.speed = speed
         self.length = length
         self.width = width
-    
+
         self.velocity = None
 
     def refresh(self, surface: pygame.Surface):
-        self.velocity = (Vector2(pygame.mouse.get_pos()) - self.position).normalize() * self.speed
+        self.velocity = (
+            Vector2(pygame.mouse.get_pos()) - self.position
+        ).normalize() * self.speed
         self.position += self.velocity * dt
         pygame.draw.line(
-            surface, (255, 255, 255),
+            surface,
+            (255, 255, 255),
             self.position,
             self.position - self.velocity.normalize() * self.length,
-            self.width
+            self.width,
         )
+
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
