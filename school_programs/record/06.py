b075d5a81e402f926e11e01086fa7b84f99cde1d
Faseeh School <faseeh1080@gmail.com>
2025-08-26 14:01:00 +0530

    Create 06.py

--- school_programs/record/06.py
+++ school_programs/record/06.py
@@ -1,2 +1,17 @@
 # Demonstrating exception handling.
 
+def calculateWeeks():
+    try:
+        days = int(input("Enter no. of days: "))
+        weeks = days // 7
+
+    except ValueError:
+        print("Enter integers only.")
+
+    else:
+        print(f"No. of weeks in {days} days: {weeks}")
+
+    finally:
+        print("Thank you.")
+
+calculateWeeks()
