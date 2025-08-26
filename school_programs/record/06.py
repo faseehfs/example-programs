df9b6c53c711b67f75f79b2480411931f1328c51
Faseeh School <faseeh1080@gmail.com>
2025-08-26 14:04:37 +0530

    Edit 06.py

--- school_programs/record/06.py
+++ school_programs/record/06.py
@@ -2,14 +2,14 @@
 
 def calculateWeeks():
     try:
-        days = int(input("Enter no. of days: "))
+        days = int(input("Number of days: "))
         weeks = days // 7
 
     except ValueError:
         print("Enter integers only.")
 
     else:
-        print(f"No. of weeks in {days} days: {weeks}")
+        print(f"Number of weeks in {days} days: {weeks}")
 
     finally:
         print("Thank you.")
