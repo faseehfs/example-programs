6877519735e0e9520aaf5d0ec05e69eb578fe6c3
FaseehFS <faseeh1080@gmail.com>
2026-02-14 21:44:01 +0530

    Create insert_node function

--- linked_lists/python/linked_list.py
+++ linked_lists/python/linked_list.py
@@ -7,7 +7,7 @@ class Node:
 def print_LL(head):
     current_node = head
     while current_node:
-        print(current_node.data, end = " -> ")
+        print(current_node.data, end=" -> ")
         current_node = current_node.next
     print("None")
 
@@ -19,7 +19,7 @@ def delete_node(head, node_to_delete):
     current_node = head
     while current_node.next and current_node.next != node_to_delete:
         current_node = current_node.next
-    
+
     if current_node.next is None:
         return head
 
@@ -27,19 +27,39 @@ def delete_node(head, node_to_delete):
     return head
 
 
-node1 = Node(11)
-node2 = Node(12)
-node3 = Node(13)
-node4 = Node(14)
-node5 = Node(15)
+def insert_node(head, new_node, position):
+    if position == 1:
+        new_node.next = head
+        return new_node
 
+    current_node = head
+    for _ in range(position - 2):
+        if current_node is None:
+            break
+        current_node = current_node.next
+
+    new_node.next = current_node.next
+    current_node.next = new_node
+    return head
+
+
+node1 = Node(1)  # This is the head.
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+node5 = Node(5)
 node1.next = node2
 node2.next = node3
 node3.next = node4
 node4.next = node5
 
+print("Created linked list:")
 print_LL(node1)
-print_LL(node3)
 
+print("Deleting node 3:")
 node1 = delete_node(node1, node3)
+print_LL(node1)
+
+print("Inserting a node at index 2 with data 3:")
+node1 = insert_node(node1, Node(3), 2)
 print_LL(node1)
