0c1cd99829227f754891225839c1e003af7a2f0b
FaseehFS <faseeh1080@gmail.com>
2026-02-21 20:38:51 +0530

    Create JavaScript intro

---
+++ javascript/intro.js
@@ -0,0 +1,8 @@
+let myNum1 = 1;
+let myNum2 = 2.0;
+
+console.log(myNum1, "is a", typeof myNum1);
+console.log(myNum2, "is also a", typeof myNum2);
+console.log(`So is ${2 + 1}.`);
+
+console.log("\nBye!");
